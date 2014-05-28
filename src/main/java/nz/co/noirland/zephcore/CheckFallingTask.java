26163a8f3e7ca0305bc80b182b7395a316988e3c
ZephireNZ <brynleymcdonald@gmail.com>
2014-05-28 21:15:53 +1200

    Move CheckFallingTask from WGFlyFlag and FlyPotions

---
+++ src/main/java/nz/co/noirland/zephcore/CheckFallingTask.java
@@ -0,0 +1,35 @@
+package nz.co.noirland.zephcore;
+
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.UUID;
+
+class CheckFallingTask extends BukkitRunnable {
+
+    private UUID player;
+
+    public CheckFallingTask(UUID player) {
+        this.player = player;
+    }
+
+    public void run() {
+        Player p = Util.player(player).getPlayer();
+        if(p == null) {
+            cancel();
+            return;
+        }
+
+        if(p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR) { // Check if player has stopped falling
+            p.setFallDistance(0f);
+            this.cancel();
+        }
+    }
+
+    public void start() {
+        runTaskTimer(ZephCore.inst(), 0L, 1L);
+    }
+
+}
