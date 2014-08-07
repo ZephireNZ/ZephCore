e1e41111a25b47b5079c04bd9854159517ba06cd
ZephireNZ <brynleymcdonald@gmail.com>
2014-08-07 22:32:28 +1200

    Callback API. Execute something in a Thread, and then return with something in thread. This handles some of the scheduling of the task.

---
+++ core/src/main/java/nz/co/noirland/zephcore/Callback.java
@@ -0,0 +1,24 @@
+package nz.co.noirland.zephcore;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * An abstract class designed to make callbacks easier.
+ * Most useful when wanting to do something Async, and then continue back in sync.
+ */
+public abstract class Callback extends BukkitRunnable {
+
+    Plugin plugin;
+
+    public Callback(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Schedule this Callback for running on the next tick.
+     */
+    public void schedule() {
+        this.runTask(plugin);
+    }
+}
